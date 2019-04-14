02b61d54d874f26d8e325a7550fdb87899fd2baf
G. Puetz <cobragp@hotmail.com>
2019-04-14 08:34:34 +0200

    Added forward method, modified init method according to needs

--- neural_network.py
+++ neural_network.py
@@ -1,15 +1,27 @@
 import numpy as np
 
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
 class SigmoidNeuralNetwork:
 
     def __init__(self, input_layer_size, hidden_layers_size, output_layer_size):
-        self.layers = [np.array([0.0 for i in range(input_layer_size)]),
-                            np.array([0.0 for i in range(hidden_layers_size[layer])])
-                            for layer in len(hidden_layers_size)],
-                            np.array([0.0 for i in range(output_layer_size)])
-        self.weights = [np.random.random(len(layer1),len(layer2))
-                        for layer1,layer2 in self.layers.zip(self.layers[1:])]
+        self.layer_sizes = [input_layer_size] + hidden_layers_size + [output_layer_size]
+        self.num_layers = len(self.layer_sizes)
+        self.weights = [np.random.rand(layer1, layer2)
+                        for layer1,layer2 in zip(self.layer_sizes, self.layer_sizes[1:])]
 
-    @staticmethod
-    def signoid(x):
-        return 
+    def forward(self, input):
+        self.layers = [input]
+        prev_layer = self.layers[0]
+        for weights in self.weights:
+            self.layers.append(sigmoid(np.dot(prev_layer,weights)))
+            prev_layer = self.layers[-1]
+
+
+
+sigmoid_network = SigmoidNeuralNetwork(2,[3],1)
+#sigmoid_network.weights = [np.array([[1,2,3],[4,5,6]]),np.array([[7],[8],[9]])]
+sigmoid_network.forward(np.array([0.1,0.2]))
+print(sigmoid_network.weights)
+print(sigmoid_network.layers)
