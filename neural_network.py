90091bbda57b3f389c902fb5005b5cd9f972f20c
G. Puetz <cobragp@hotmail.com>
2019-04-15 22:10:54 +0200

    Added gradient computation, cost function and tests Missing gradient computation test

--- neural_network.py
+++ neural_network.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+def sigmoidprime(x):
+    return np.exp(-1) / (1+np.exp(-x))**2
+
 class SigmoidNeuralNetwork:
 
     def __init__(self, input_layer_size, hidden_layers_size, output_layer_size):
@@ -12,16 +15,61 @@ class SigmoidNeuralNetwork:
                         for layer1,layer2 in zip(self.layer_sizes, self.layer_sizes[1:])]
 
     def forward(self, input):
-        self.layers = [input]
-        prev_layer = self.layers[0]
+        self.ps = [input]
+        self.zs = []
+        prev_layer = self.ps[0]
         for weights in self.weights:
-            self.layers.append(sigmoid(np.dot(prev_layer,weights)))
-            prev_layer = self.layers[-1]
+            self.zs.append(np.dot(prev_layer,weights))
+            self.ps.append(sigmoid(self.zs[-1]))
+            prev_layer = self.ps[-1]
+        return self.ps[-1]
+
+    def cost_function(self, outputs):
+        error = 1/2*sum(np.dot(result-output,result-output) for result, output in zip(self.ps[-1], outputs))
+        return error
+
+
+    def cost_function_gradient(self, outputs):
+        gradients = []
+        delta = [np.mulitply(outputs - self.ps[-1], signmoidprime(self.zs[-1]))]
+        for a, z, w in zip(reversed(self.ps[:-1]), reversed(self.zs), reversed(self.weights)):
+            delta = np.multiply(np.dot(prev_delta, w.T), sigmoidprime(z))
+            gradients.append(np.multiply(a.T, delta))
+        return reversed(gradients)
 
 
 
-sigmoid_network = SigmoidNeuralNetwork(2,[3],1)
-#sigmoid_network.weights = [np.array([[1,2,3],[4,5,6]]),np.array([[7],[8],[9]])]
-sigmoid_network.forward(np.array([0.1,0.2]))
-print(sigmoid_network.weights)
-print(sigmoid_network.layers)
+class Test:
+    def __init__(self):
+        self.sigmoid_network = SigmoidNeuralNetwork(2,[3],1)
+        self.sigmoid_network.weights = [np.array([[1.6,2.2,3.7],[1.3,1.9,3.2]]),np.array([[2.5],[4.2],[2.1]])]
+        self.inputs = [np.array([0.1,0.2]),np.array([0.3,0.4])]
+
+    def forward(self):
+        self.sigmoid_network.forward(self.inputs)
+        if abs(self.sigmoid_network.ps[-1][0][0] -0.996858182) < 0.0000001:
+            print('Forward passed')
+        else:
+            print('Forward failed')
+
+    def cost_function(self):
+        cost = self.sigmoid_network.cost_function([np.array([0.98]),np.array([0.997])])
+        if abs(cost - 0.000144159) < 0.000001:
+            print('Cost_function passed')
+        else:
+            print('Cost_function failed')
+
+    def cost_function_gradient(self):
+        epsilon = 1e-4
+        network_plus = self.sigmoid_network = SigmoidNeuralNetwork(2,[3],1)
+        network_plus.weights = self.sigmoid_network.weights + epsilon
+        network_plus.forward(self.inputs)
+        network_minus = self.sigmoid_network = SigmoidNeuralNetwork(2,[3],1)
+        network_minus.weights = self.sigmoid_network.weights - epsilon
+        network_minus.forward(self.inputs)
+
+
+
+test = Test()
+test.forward()
+test.cost_function()
